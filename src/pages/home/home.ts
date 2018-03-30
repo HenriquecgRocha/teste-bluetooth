ffdce5313140ea766171ad8cd2cce8a4620fcd53
henrique <henriquecg.rocha@gmail.com>
2018-03-29 22:02:51 -0300

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController,ModalController,AlertController } from 'ionic-angular';
-import {PrintProvider} from '../../providers/print/print';
-import {PrinterListModalPage} from '../printer-list-modal/printer-list-modal';
+import { NavController, ModalController, AlertController } from 'ionic-angular';
+import { PrintProvider } from '../../providers/print/print';
+import { PrinterListModalPage } from '../printer-list-modal/printer-list-modal';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
 
 
 @Component({
@@ -9,88 +10,88 @@ import {PrinterListModalPage} from '../printer-list-modal/printer-list-modal';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  selectedPrinter:any=[];
+  selectedPrinter: any = [];
 
-  constructor(public navCtrl: NavController,private modalCtrl:ModalController,
-    private printProvider:PrintProvider,
-    private alertCtrl:AlertController) {
+  constructor(public navCtrl: NavController, private modalCtrl: ModalController,
+    private printProvider: PrintProvider,
+    private bluetoothSerial: BluetoothSerial,
+    private alertCtrl: AlertController) {
 
   }
 
+  connectionBluetooth () {
+    this.bluetoothSerial.isEnabled().then((result) => {
+      console.log(result)
+    })
+  }
 
-  listBTDevice()
-  {
-    this.printProvider.searchBt().then(datalist=>{
-      
+  listBTDevice() {
+    this.printProvider.searchBt().then(datalist => {
+
       //1. Open printer select modal
-      let abc=this.modalCtrl.create(PrinterListModalPage,{data:datalist});
-      
-      //2. Printer selected, save into this.selectedPrinter
-      abc.onDidDismiss(data=>{
-        this.selectedPrinter=data;
+      let abc = this.modalCtrl.create(PrinterListModalPage, { data: datalist });
 
-        let xyz=this.alertCtrl.create({
-          title: data.name+" selected",
-          buttons:['Dismiss']
+      //2. Printer selected, save into this.selectedPrinter
+      abc.onDidDismiss(data => {
+        this.selectedPrinter = data;
+
+        let xyz = this.alertCtrl.create({
+          title: data.name + " selected",
+          buttons: ['Dismiss']
         });
         xyz.present();
 
       });
-      
+
       //0. Present Modal
       abc.present();
 
-    },err=>{
-      console.log("ERROR",err);
-      let mno=this.alertCtrl.create({
-        title:"ERROR "+err,
-        buttons:['Dismiss']
+    }, err => {
+      console.log("ERROR", err);
+      let mno = this.alertCtrl.create({
+        title: "ERROR " + err,
+        buttons: ['Dismiss']
       });
       mno.present();
     })
 
   }
 
-  testConnectPrinter()
-  {
-    var id=this.selectedPrinter.id;
-    if(id==null||id==""||id==undefined)
-    {
+  testConnectPrinter() {
+    var id = this.selectedPrinter.id;
+    if (id == null || id == "" || id == undefined) {
       //nothing happens, you can put an alert here saying no printer selected
     }
-    else
-    {
-      let foo=this.printProvider.connectBT(id).subscribe(data=>{
-        console.log("CONNECT SUCCESSFUL",data);
+    else {
+      let foo = this.printProvider.connectBT(id).subscribe(data => {
+        console.log("CONNECT SUCCESSFUL", data);
 
-        let mno=this.alertCtrl.create({
-          title:"Connect successful",
-          buttons:['Dismiss']
+        let mno = this.alertCtrl.create({
+          title: "Connect successful",
+          buttons: ['Dismiss']
         });
         mno.present();
 
-      },err=>{
-        console.log("Not able to connect",err);
-        let mno=this.alertCtrl.create({
-          title:"ERROR "+err,
-          buttons:['Dismiss']
+      }, err => {
+        console.log("Not able to connect", err);
+        let mno = this.alertCtrl.create({
+          title: "ERROR " + err,
+          buttons: ['Dismiss']
         });
         mno.present();
       });
     }
   }
 
-  testPrinter()
-  {
-    var id=this.selectedPrinter.id;
-    if(id==null||id==""||id==undefined)
-    {
+  testPrinter() {
+    var id = this.selectedPrinter.id;
+    if (id == null || id == "" || id == undefined) {
       //nothing happens, you can put an alert here saying no printer selected
     }
-    else
-    {
-      let foo=this.printProvider.testPrint(id);
+    else {
+      let foo = this.printProvider.testPrint(id);
     }
   }
 
 }
+
